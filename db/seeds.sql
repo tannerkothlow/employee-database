2537f8e29cf19860550481a5330c4bb0eb3d83eb
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-19 10:57:31 -0600

    Fixed seeding a manager later that the emp they manage

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,25 +3,27 @@ INSERT INTO department (name)
 VALUES  ('Produce'),
         ('Bakery'),
         ('Meat'),
-        ('Deli')
+        ('Deli');
 
--- Initialize 6 roles
+-- Initialize 7 roles
 INSERT INTO role (title, salary, department_id)
 VALUES  ('Stocker', 26000, 1),
-        ('Baker' 32000, 2),
+        ('Baker', 32000, 2),
         ('Wrapper', 29000, 2),
         ('Cutter', 43000, 3),
         ('Rotisserie', 32000, 4),
-        ('Manager', 65000, 4)
+        ('Deli Manager', 65000, 4),
+        ('Fresh Manager', 54000, 2);
 
 -- Initialize 10 employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('Greg', 'Gregson', 3, 3),
-        ('Jim', 'Jenson' 1, null),
-        ('Blake', 'Blakeson', 6, null),
-        ('Tanner', 'Kothlow', 5, 3),
-        ('Zoe', 'Kothlow', 4, 3),
+VALUES  ('Blake', 'Blakeson', 6, null),
+        ('Greg', 'Gregson', 6, null),
+        ('Jim', 'Jenson', 7, null),
+        ('Tanner', 'Kothlow', 5, 1),
+        ('Zoe', 'Kothlow', 4, 1),
         ('Alex', 'Soltero', 4, 4),
-        ('Maddie', 'Mewo', 2, null),
-        ('Diego', 'Grill', 2, null),
-        ('Doug', 'Douglas', 1, null)
+        ('Maddie', 'Mewo', 3, 3),
+        ('Diego', 'Grill', 2, 3),
+        ('Doug', 'Douglas', 1, 2);
+        

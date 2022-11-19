3bd5036f13920805a9244430b8f7e516508fb5a3
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-19 11:23:06 -0600

    Connection to bigstore_db established

--- helpers/mysql.js
+++ helpers/mysql.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2');
 
+require('dotenv').config({ path: '../.env' });
+
 // View all departments (polymorph to send back an array)
 // View all roles (polymorph to send back an array)
 // View all employees (polymorph to send back an array, polymorph to filter ONLY managers)
@@ -7,6 +9,16 @@ const mysql = require('mysql2');
 // Add a role
 // Add an employee
 // Update and employee role
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connect to the bigstore_db database.`)
+);
 
 class DBFunc {
 
